8bad845dbd3eb66d69f47828a660409fb5e9f773
subbat9710 <tula.subba1991@gmail.com>
2017-03-14 20:00:00 -0400

    update

--- playgame.rb
+++ playgame.rb
@@ -18,23 +18,39 @@ sleep 1
 currentplayer = player1
 console = Console.new  
 #random = Random.new
-	loop do console.display_board(board.board)
-	print "#{name1}, enter your move: "
-	move = gets.chomp.to_i  
-	#move = random.move
-	board.set_position(move, "X")
+while console.game_over?
 	console.display_board(board.board)
-    sleep 1
-    #console.display_board(board.board)
-    print "#{name2}, enter your move: "
-    move = gets.chomp.to_i
-    board.set_position(move, "O")
-    #console.display_board(board.board)
+	move = gets.chomp.to_i
+	board.set_position(move, "X")
+	if board.stub_winner?
+		console.display_board(board.board)
+		puts "#{name1} has won! Congratulations."
+    elsif console.switch_player(currentplayer)
+			puts "Better luck next time, #{name1}."
+    elsif board.tie
+        console.display_board(board.board)
+        puts "game is tie"
+    else console.switch_player(currentplayer)
+    end
 end
 
-# console = Console.new
-#     board = Board.new
-#     board.set_position(1, "x")
-#     #p board.board 
 
-#     console.display_board(board.board)
+# console.display_board(board.board) #this line display the board in the terminals.
+ 	#print "#{name1}, enter your move: "
+ 	#move = gets.chomp.to_i  
+# 	#move = random.move
+# 	board.set_position(move, "X")
+# 	console.display_board(board.board)
+#     sleep 1
+#     #console.display_board(board.board)
+#     print "#{name2}, enter your move: "
+#     move = gets.chomp.to_i
+#     board.set_position(move, "O")
+#     #console.display_board(board.board)
+
+ # console = Console.new
+ #     board = Board.new
+ #     board.set_position(1, "x")
+ #     p board.board 
+
+ #     console.display_board(board.board)
