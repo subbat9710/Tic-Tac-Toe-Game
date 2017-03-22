b6affcb00af7d2f6ce5f8fba039be929e4d361e6
subbat9710 <tula.subba1991@gmail.com>
2017-03-22 12:12:32 -0400

    Test2

--- test_unbeatable.rb
+++ test_unbeatable.rb
@@ -1,6 +1,6 @@
 require "minitest/autorun"
 require_relative "unbeatable.rb"
-require_relative "board.rb"
+require_relative "console.rb"
 
 class TestUnbeatable < Minitest::Test
 
@@ -9,8 +9,15 @@ class TestUnbeatable < Minitest::Test
 		assert_equal("O", ai.marker)
 	end
 	def test_position_zero_for_O
-		ai = Unbeatable.new("O")
-		board = ["", "x", "x", "o", "", "", "", "", ""]
-		assert_equal(0, ai.get_move(board))
+		ai = Unbeatable.new("o")
+		board = ["", "o", "o", "o", "", "", "", "", ""]
+		game = Console.new(board)
+		assert_equal(0, ai.check_win(board, ai))
+	end
+	def test_position_one_for_O
+		ai = Unbeatable.new("o")
+		board = ["o", "", "x", "o", "x", "", "", "", ""]
+		game = Console.new(board)
+		assert_equal(6, ai.check_win(board, ai))
 	end
 end
--- unbeatable.rb
+++ unbeatable.rb
@@ -1,13 +1,95 @@
 require_relative "board.rb"
 
 class Unbeatable
-	attr_reader :marker
+	attr_accessor :marker, :currentplayer
 
 	def initialize(marker)
 		@marker = marker
+		@currentplayer = currentplayer
 	end
 	def get_move(board)
-		ai_marker = marker
-		0
+		if currentplayer.marker == "O"
+			aiplayer = "X"
+		else
+			aiplayer = "O"
+		end
+    end
+	def check_win(board, currentplayer)
+		check_win(board, currentplayer)
 	end
+	def block_win(board, currentplayer)
+		win_or_block(board, currentplayer)
+	end
+	def map_board(board)
+		win_combinations = [
+			                 [board[0], board[1], board[2]],
+			                 [board[3], board[4], board[5]],
+			                 [board[6], board[7], board[8]],
+			                 [board[0], board[3], board[6]],
+			                 [board[1], board[4], board[7]],
+			                 [board[2], board[5], board[8]],
+			                 [board[0], board[4], board[8]],
+			                 [board[2], board[4], board[6]]
+		                    ]
+	    end
+	def check_win(board, currentplayer)
+		win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+        win_array1 = map_board(board)
+		move = 1
+
+		win_array1.each_with_index do |element, indx|
+			# puts "#{element.inspect}"
+			# puts "#{element.count(currentplayer.marker)}"
+			if element.count(currentplayer.marker) == 2 && element.count("") == 1
+				winner_or_block = element.index("")
+				puts "#{winner_or_block}"
+				move = win_array[indx][winner_or_block]
+			end
+		end
+        move
+	end
+# 	def win_or_block(board, currentplayer)
+# 		win_array = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+#         win_or_block_array = map_board(board)
+# 		move = 1
+
+# 		win_or_block_array.each_with_index do |element, indx|
+# 			# puts "#{element.inspect}"
+# 			# puts "#{element.count(currentplayer.marker)}"
+# 			if element.count(currentplayer.marker) == 2 && element.count("") == 1
+# 				winner_or_block = element.index("")
+# 				puts "#{winner_or_block}"
+# 				move = win_array[indx][winner_or_block]
+# 			end
+# 		end
+#         move
+# 	end
+# end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
